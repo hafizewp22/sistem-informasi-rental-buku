559255f02a73cc3975b5a85615eef4ccca8aa321
Hafiz Elfia Wedo Putra <52981264+hafizewp22@users.noreply.github.com>
2023-08-28 13:15:08 +0700

    add middleware and log out

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function profile(Request $request) {
+        // $request->session()->flush();
+
+        return view('profile');
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -26,17 +26,22 @@
 </style>
 
 <body>
-    <div class="main d-flex justify-content-center align-items-center">
+    <div class="main d-flex flex-column justify-content-center align-items-center">
+        @if (session('status'))
+            <div class="alert alert-danger">
+                {{ session('message') }}
+            </div>
+        @endif
         <div class="login-box">
             <form action="" method="post">
                 @csrf
                 <div>
                     <label for="username" class="form-label">Username</label>
-                    <input type="text" name="username" id="username" class="form-control">
+                    <input type="text" name="username" id="username" class="form-control" required>
                 </div>
                 <div>
                     <label for="password" class="form-label">Password</label>
-                    <input type="password" name="password" id="password" class="form-control">
+                    <input type="password" name="password" id="password" class="form-control" required>
                 </div>
                 <div>
                     <button type="submit" class="btn btn-primary form-control">Login</button>
