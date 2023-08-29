369ed244b5fba36e5ed55705df72b2e07149d3fb
Hafiz Elfia Wedo Putra <52981264+hafizewp22@users.noreply.github.com>
2023-08-29 11:04:42 +0700

    update register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -47,7 +47,7 @@
                     <button type="submit" class="btn btn-primary form-control">Login</button>
                 </div>
                 <div class="text-center">
-                    <a href="register">Sign Up</a>
+                    don't have account? <a href="register">Sign Up</a>
                 </div>
             </form>
         </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,8 +7,54 @@
     <title>Rental Buku | Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
+
+<style>
+    .main {
+        height: 100vh;
+        box-sizing: border-box;
+    }
+
+    .register-box {
+        width: 500px;
+        border: solid 1px;
+        padding: 30px;
+    }
+
+    form div {
+        margin-bottom: 15px;
+    }
+</style>
+
 <body>
-    
+    <div class="main d-flex flex-column justify-content-center align-items-center">
+        <div class="register-box">
+            <form action="" method="post">
+                @csrf
+                <div>
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name="username" id="username" class="form-control" required>
+                </div>
+                <div>
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password" class="form-control" required>
+                </div>
+                <div>
+                    <label for="phone" class="form-label">Phone</label>
+                    <input type="text" name="phone" id="phone" class="form-control">
+                </div>
+                <div>
+                    <label for="address" class="form-label">Address</label>
+                    <textarea name="address" id="address" class="form-control" rows="5" required></textarea>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary form-control">Register</button>
+                </div>
+                <div class="text-center">
+                    Have account? <a href="login">Login</a>
+                </div>
+            </form>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
