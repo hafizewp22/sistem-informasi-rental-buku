9cd5a40a1b4208977e71ecd834e0d8ec828078f7
Hafiz Elfia Wedo Putra <52981264+hafizewp22@users.noreply.github.com>
2023-08-30 10:42:14 +0700

    add template layout

---
+++ resources/views/layouts/mainlayout.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rental Buku | @yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<style>
+    .main {
+        height: 100vh;
+    }
+
+    .sidebar {
+        background: rgb(96, 95, 95);
+        color: #fff;
+    }
+
+    .sidebar a {
+        color: #fff;
+        text-decoration: none;
+        display: block;
+        padding: 20px 10px;
+    }
+
+    .sidebar a:hover {
+        background: #b0aaaa;
+        color: #000000;
+    }
+</style>
+
+<body>
+
+    <div class="main d-flex flex-column justify-content-between">
+        <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Rental Buku</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
+
+        <div class="body-content h-100">
+            <div class="row g-0 h-100">
+                <div class="sidebar col-lg-2 collapse d-lg-block" id="navbarTogglerDemo02">
+                    @if (Auth::user()->role_id == 1)
+                        <a href="dashboard">Dashboard</a>
+                        <a href="books">Books</a>
+                        <a href="#">Categories</a>
+                        <a href="#">Users</a>
+                        <a href="#">Rent Log</a>
+                        <a href="logout">Logout</a>
+                    @else
+                        <a href="profile">Profile</a>
+                        <a href="logout">logout</a>
+                    @endif
+
+                </div>
+                <div class="content p-5 col-lg-10">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
