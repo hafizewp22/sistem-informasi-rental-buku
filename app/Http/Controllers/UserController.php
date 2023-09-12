39dab8f8aa1d970c776fdba3a1b114612e4905d9
Hafiz Elfia Wedo Putra <52981264+hafizewp22@users.noreply.github.com>
2023-09-12 09:45:14 +0700

    Add view data user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function profile(Request $request) {
-        // $request->session()->flush();
+    public function profile() {
 
         return view('profile');
     }
 
     public function index() {
-        return view('user');
+        $users = User::where('role_id', 2)->get();
+
+        return view('user', compact('users'));
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -3,5 +3,50 @@
 @section('title', 'User')
 
 @section('content')
-    ini halaman User
+    <h1>User List</h1>
+
+    <div class="mt-5 d-flex justify-content-end">
+        <a href="user-ben" class="btn btn-secondary me-3">View Banned User</a>
+        <a href="user-view" class="btn btn-primary">View Registered User</a>
+    </div>
+
+    <div class="mt-5">
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+    </div>
+
+    <div class="my-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Username</th>
+                    <th>Phone</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $item)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $item->username }}</td>
+                        <td>
+                            @if ($item->phone == null && $item->phone == '')
+                                -
+                            @else
+                                {{ $item->phone }}
+                            @endif
+                        </td>
+                        <td>
+                            <a href="/user-detail/{{$item->username}}">Detail</a>
+                            <a href="/user-ban/{{$item->username}}">Ban User</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
