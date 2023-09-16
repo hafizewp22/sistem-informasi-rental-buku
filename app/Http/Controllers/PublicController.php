eff72dd5356e200aa3524dabbae0cb23f9bc2358
Hafiz Elfia Wedo Putra <52981264+hafizewp22@users.noreply.github.com>
2023-09-16 15:41:47 +0700

    add search list book

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
-    public function index() {
-        $books = Book::all();
-        return view('book-list', compact('books'));
+    public function index(Request $request) {
+        $categories = Category::all();
+
+        if ($request->category || $request->title) {
+            // $books = Book::join('book_categorys', 'books.id', '=', 'book_categorys.book_id')
+            // ->join('categories', 'book_categorys.category_id', '=', 'categories.id')
+            // ->orWhere('books.title', '=', '%'.$request->title.'%')
+            // ->orWhere('categories.id', '=', $request->category)
+            // ->get();
+            $books = Book::where('title', 'like', '%'.$request->title.'%')
+                        ->orWhereHas('categories', function($q) use($request) {
+                            $q->where('categories.id', $request->category);
+                        })->get();
+                       
+            // $books = Book::whereHas('categories', function($q) use($request) {
+            //     $q->where('categories.id', $request->category);
+            // })->get();
+        } else {
+            $books = Book::all();
+        }
+
+        return view('book-list', compact('books', 'categories'));
     }
 }
